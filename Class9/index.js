8a0b22677a6027babba9bb26ee30d1b3ed6bf613
SVB808 <shruthavbhat@gmail.com>
2024-02-10 21:12:09 +0530

    todo application

--- Class9/index.js
+++ Class9/index.js
@@ -4,6 +4,7 @@ let todos = JSON.parse(localStorage.getItem("todos")) || [];
 const todoData = document.getElementById("todoData");
 const todoForm = document.getElementById("todoForm");
 const alertMsg = document.getElementById("alertMsg");
+const updateTask = document.getElementById("updateTask");
 
 todoForm.addEventListener("submit", (e) => { 
     e.preventDefault();
@@ -17,24 +18,43 @@ todoForm.addEventListener("submit", (e) => {
     todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(todos));
     renderTodos();
+    todoForm.reset();
 });
 
+const deleteTodo = (id) => {
+    localStorage.setItem("todos",JSON.stringify(todos.filter((todo)=>todo.id !== id)));
+    todos=todos.filter((todo)=>todo.id !== id);
+    renderTodos();
+}
+
+const editTodo = (id) => {
+    document.getElementById("taskName").value = todos[id -1].name;
+    document.getElementById("priority").value = todos[id -1].priority;
+
+    updateTask.addEventListener("click",(e) => {
+        e.preventDefault();
+        todos[id-1].name = document.getElementById("taskName").value;
+        todos[id-1].priority = document.getElementById("priority").value;
+        localStorage.setItem("todos",JSON.stringify(todos));
+        renderTodos();
+        todoForm.reset();
+    })
+} 
 
 const renderTodos = () => {
     let data = "";
     todos.forEach((todo) => {
-        data += `
-        <tr>
-            
+        const pc = todo.priority === "High"?"danger":todo.priority === "Medium"?"warning":"primary";
+        data += `<tr>           
             <td>${todo.name}</td>
-            <td>${todo.priority}</td>
+            <td><span class = "badge bg-${pc}">${todo.priority}</td>
             <td>
-                <button class="btn btn-success" onclick="deleteTodo(${todo.id})">Edit</button>
-                <button class="btn btn-danger" onclick="deleteTodo(${todo.id})">Delete</button>
+                <button class="btn btn-success" onclick="editTodo(${todo.id})"><i class="fa-solid fa-pen"></i></button>
+                <button class="btn btn-danger" onclick="deleteTodo(${todo.id})"><i class="fa-solid fa-trash"></i></button>
             </td>
         </tr>`;
     });
     document.getElementById("todoListTbody").innerHTML = data;
 };
 
-// renderTodos();
+renderTodos();
