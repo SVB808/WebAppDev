3fc13b45be5bb819d49e307d2a2513cf9e43fbbe
SVB808 <shruthavbhat@gmail.com>
2024-02-24 17:52:24 +0530

    props

--- Class12/firstreact/src/App.js
+++ Class12/firstreact/src/App.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import './App.css';
+import { useState } from 'react';
 
-function MyButton(){
-  return <button>Click me</button>;
+function MyButton(props){
+  const {count, handleClick} = props;
+  return <button onClick={handleClick}>Clicked {count} times</button>;
 }
 
 function App() {
   const headerClass='heading';
   //user map
+  const [count, setCount] = useState(0);
+  function handleClick(){
+    setCount(count+1);
+  }
   const user = {
-    name:'John',
+    name:'Joseph',
     age:25,
-    address:'USA'
+    address:'United States of America',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize:200
   };
+
+const courses = [
+  {id:1, name:'React',isElective:true},
+  {id:1, name:'Angular',isElective:false},
+  {id:1, name:'Vue',isElective:true}
+]
+
+  let content;
+  if(user.age>=18){
+    content=<MyButton count={count} handleClick={handleClick}/>;
+  }
   return ( 
     <>
       <h1 className={headerClass}>React App</h1>
+      <img src={user.imageUrl} alt="user" style={{width:user.imageSize, height:user.imageSize}}/>
       <h2>By {user.name} </h2>
-      <MyButton/>
+      <MyButton count={count} handleClick={handleClick}/>
+      {content}
+<ul>
+  {courses.map((course) => (
+    <li key={course.id} style={{color:course.isElective?'blue':'green'}}>
+      {course.name}
+    </li>
+  ))}
+</ul>
     </>
    );
 }
